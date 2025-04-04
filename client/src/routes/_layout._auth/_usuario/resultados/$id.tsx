c0f71f4b3ad043eb66fda21064d356f14d54a1c6
felipeazs <felipeazs@gmail.com>
2025-04-04 10:14:20 -0300

    resultdos.id responsiveness

--- client/src/routes/_layout._auth/_usuario/resultados/$id.tsx
+++ client/src/routes/_layout._auth/_usuario/resultados/$id.tsx
@@ -35,7 +35,7 @@ function RouteComponent() {
 	}, [resultado])
 
 	return (
-		<div className="space-y-6">
+		<div className="w-full space-y-6">
 			<div className="flex items-center justify-between">
 				<div>
 					<h1 className="text-3xl font-bold tracking-tight">Evaluacion</h1>
