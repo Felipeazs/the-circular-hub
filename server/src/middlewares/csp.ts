07cfbb367cb8e601ba353740b12e6d8f33df1d70
felipeazs <felipeazs@gmail.com>
2025-04-04 10:12:05 -0300

    csp

--- server/src/middlewares/csp.ts
+++ server/src/middlewares/csp.ts
@@ -15,6 +15,7 @@ export const CSP_RULES = {
 			"'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
 			"'sha256-knPAv6T1m9aFSc0s3vSMuh3Kdxy//BumDVqYOSPzYqE='",
 			"'sha256-nzTgYzXYDNe6BAHiiI7NNlfK8n/auuOAhh2t92YvuXo='",
+			"'sha256-kAApudxpTi9mfjlC9lC8ZaS9xFHU9/NLLbB173MU7SU='",
 		],
 	},
 	permissionsPolicy: {
